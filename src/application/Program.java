e58bc882057fff7b48b3401264301d0a0eea9e40
Matheus Silveira <matheus.silveiraw@gmail.com>
2023-08-22 21:01:36 -0300

    básico do menu

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,25 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		//pronto pra utilizar
+		
+		int menu = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		while (menu != 9) {
+			
+			System.out.println("[0] - Adicionar um produto");
+			System.out.println("[1] - Remover um produto");
+			System.out.println("[2] - Atualizar um produto");
+			System.out.println("[3] - Listar produtos produtos");
+			System.out.println("[9] - Sair do programa");
+			menu = sc.nextInt();
+			
+		} 
 
 	}
 }
